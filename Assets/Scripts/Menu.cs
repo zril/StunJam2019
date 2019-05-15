f973b9242c3c922a6b0271a2f4a433f37230b3eb
cyril.savary.42@gmail.com <fzefre@fezrf.com>
2019-05-16 01:57:26 +0200

    levels

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -54,14 +54,16 @@ public class Menu : MonoBehaviour
 			{ "Niveau 3", "tati titati titotiteta titotetitatu tu tu" },
 			{ "Sonic", "It's me, Mario" },
 			{ "Pikachu", "PIEEEEEEEEERRE" },
-			{ "Morse", "Le code Morse international, est un code inventé en 1832 pour la télégraphie et permettant de transmettre un texte à l’aide de séries d’impulsions courtes et longues, qu’elles soient produites par des signes, une lumière, un son ou un geste." },
 			{ "Fibonacci", "1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, etc" },
 			{ "Braddock", "Je mets les pieds où je veux Little John, et c'est souvent dans la gueule." },
 			{ "Latin", "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
 			{ "Camarade", "Ami, entends tu le vol noir des corbeaux sur nos plaines" },
 			{ "Coiffeur", "Belle journée. On a du mal à croire qu'on est en mai." },
+            { "Your base", "All your base are belong to us. You have no chance to survive make your time. Ha Ha Ha Ha..." },
+            { "Morse", "Le code Morse international, est un code inventé en 1832 pour la télégraphie et permettant de transmettre un texte à l’aide de séries d’impulsions courtes et longues, qu’elles soient produites par des signes, une lumière, un son ou un geste." },
             { "Tutoriel", "Aidez la mire de calibrage à convertir le texte en code MORSE déstiné aux millions d'usagers désireux de faire savoir à leurs proches les nouvelles essentielles de leurs vies, sans les inconvénients de temps et d'argent qu'impliquent l'emploi d'un messager, et parce que ça rapporte des POINTS. SAUTEZ par dessus les arcs électrostatiques résiduels et détruisez les écrans plats. Ces derniers renferment un pouvoir multimédiatique sans commune mesure, augmentant ainsi votre multiplicateur de SCORE de façon significative. Si le multiplicateur est rempli, vous passez en mode SUPER PLAY, vous devrez alors mettre en oeuvre tout ce que ce TUTORIEL vous aura appris sur le MORSE pour braver la cécité gratifiante. La fonctionnalité de HIGH SCORE ainsi que le mode VR seront disponibles dans de prochains DLC, en attendant n'hésitez pas à écrire votre HIGH SCORE sur un POST IT et à le COLLER sur l'écran." },
-            { "2 joueurs", "Utilisez le clavier pour improviser le texte" } };
+            { "2 joueurs", "Utilisez le clavier pour improviser le texte" }
+        };
 
         
 
