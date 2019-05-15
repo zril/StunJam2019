15dcbedb9f1088880580030802a7a95387a63f6a
cyril.savary.42@gmail.com <fzefre@fezrf.com>
2019-05-15 22:54:59 +0200

    correction menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -19,6 +19,8 @@ public class Menu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Global.levelText = "title";
+
         nomireTimer = 0;
         main = gameObject.GetComponent<Main>();
 
